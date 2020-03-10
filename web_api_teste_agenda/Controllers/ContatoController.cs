d8e46316212a3afd75a4beabeaaeea2d5d19f015
Anderson <anderson.assis@outlook.com>
2020-03-09 23:01:18 -0300

    Add project files.

---
+++ web_api_teste_agenda/Controllers/ContatoController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using web_api_teste_agenda.Models;
+
+namespace web_api_teste_agenda.Controllers
+{
+    public class ContatoController : ApiController
+    {
+        // mostrar os contatos
+        [HttpGet]
+        public List<Contato> MostrarContato()
+        {
+            return Contato.listaContatos;
+        }
+
+        // adicionar contato
+        [HttpPost]
+        public string AdicContato(string nome_Desejado, string telefone_Desejado)
+        {
+            // cria contato e joga na lista
+            Contato novoContato = new Contato();
+            novoContato.Nome = nome_Desejado;
+            novoContato.Telefone = telefone_Desejado;
+            Contato.listaContatos.Add(novoContato);
+
+            return "Contato adicionado!";
+        }
+
+        // deletar contato
+        [HttpDelete]
+        public void DelContato(string nome_Procurado)
+        {
+            foreach(var contato in Contato.listaContatos)
+            {
+                if(contato.Nome == nome_Procurado)
+                {
+                    Contato.listaContatos.Remove(contato);
+                    return;
+                }                
+            }
+        }
+
+        // atualizar contato
+        [HttpPut]
+        public void Atualizar(int id_Desejada)
+        {
+            foreach(var contato in Contato.listaContatos)
+            {
+                if (contato.Id == id_Desejada)
+                {
+                    //int indiceDesseContato=
+                }
+            }
+        }
+        
+    }
+}
---
+++ web_api_teste_agenda/Models/Contato.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web_api_teste_agenda.Models
+{
+    public class Contato
+    {
+        //propiedades
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+
+        // variavel para lista de contato inicializada
+        public static List<Contato> listaContatos = new List<Contato>()
+        {
+            new Contato
+            {
+                Id = 1, Nome="Anderson",Telefone="33658998"
+            },
+            new Contato
+            {
+                Id = 2, Nome="Andrei", Telefone="556553333"
+            }
+        };
+    }
+}
